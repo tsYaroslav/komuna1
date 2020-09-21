60417f39268d2ca5ea3202e55a3c3ae9b794d9bd
121yaroslav <yaroslav121mail@gmail.com>
2020-09-21 12:45:44 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -386,7 +386,7 @@ window.onload = () => {
 			bills.insertAdjacentHTML ("beforeEnd", strBill);			
 		}
 	}());
-	
+
 	if(Number(localStorage.darkMode) === 1) {
 		body.classList.add("dark-mode");
 		document.querySelector('#darkmode-switch').setAttribute("checked", "checked");
